11d9047f29e22e36859699d9ddb6269dd9d788c6
Rinnohara9803 <sagarprajapati9803@gmail.com>
2022-07-05 17:13:38 +0545

    minor changes made

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -7,24 +7,6 @@ class DashboardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        bottomNavigationBar: FluidNavBar(
-            icons: [
-              FluidNavBarIcon(
-                  icon: Icons.home, backgroundColor: Color(0xFF4285F4)),
-              FluidNavBarIcon(
-                  icon: Icons.monetization_on,
-                  backgroundColor: Color(0xFFEC4134)),
-              FluidNavBarIcon(
-                  icon: Icons.ac_unit, backgroundColor: Color(0xFFFCBA02)),
-              FluidNavBarIcon(
-                  icon: Icons.alarm, backgroundColor: Color(0xFF34A950)),
-            ],
-            onChange: (val) {},
-            style: const FluidNavBarStyle(
-                iconUnselectedForegroundColor: Colors.white)),
-      ),
-    );
+    return Scaffold();
   }
 }
