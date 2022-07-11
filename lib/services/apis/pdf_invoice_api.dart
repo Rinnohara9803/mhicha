9a3d8fcbd3b66a2a015972aed05a70222b51e941
Rinnohara9803 <sagarprajapati9803@gmail.com>
2022-07-11 11:54:03 +0545

    pdf-invoice ui-errors fixed

--- lib/services/apis/pdf_invoice_api.dart
+++ lib/services/apis/pdf_invoice_api.dart
@@ -8,6 +8,74 @@ class PdfInvoiceApi {
   static Future<File> generate(Student student) async {
     final pdf = Document();
 
+    pdf.addPage(
+      MultiPage(
+        build: (context) => [
+          Expanded(
+            flex: 2,
+            child: buildTopInvoiceLayout(),
+          ),
+          Divider(
+            thickness: 1,
+          ),
+          Expanded(
+            flex: 5,
+            child: buildMiddleInvoiceLayout(),
+          ),
+          Divider(
+            thickness: 1,
+          ),
+          Expanded(
+            flex: 5,
+            child: buildBottomInvoiceLayout(),
+          ),
+        ],
+      ),
+    );
+
+    return PdfApi.saveDocument(name: 'student1.pdf', pdf: pdf);
+  }
+
+  static Widget buildTopInvoiceLayout() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 5,
+        horizontal: 10,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  'Send Money',
+                ),
+                Text(
+                  DateFormat('yyyy-MM-dd kk:mm a').format(
+                    DateTime.now(),
+                  ),
+                ),
+                Text(
+                  'Complete',
+                ),
+              ],
+            ),
+          ),
+          Text(
+            'Rs. 50',
+            style: const TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  static Widget buildMiddleInvoiceLayout() {
     Widget dataWidget(String key, String value) {
       return Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -23,198 +91,129 @@ class PdfInvoiceApi {
       );
     }
 
-    pdf.addPage(
-      MultiPage(
-        build: (context) => [
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 5,
-                horizontal: 10,
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 5,
+        horizontal: 10,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: dataWidget('Channel:', 'GPRS'),
               ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(
-                        'Send Money',
-                      ),
-                      Text(
-                        DateFormat('yyyy-MM-dd – kk:mm a').format(
-                          DateTime.now(),
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 2,
-                          horizontal: 7,
-                        ),
-                        child: Text(
-                          'Complete',
-                        ),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    'Rs. 50',
-                    style: const TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
-                ],
+              SizedBox(
+                width: 5,
               ),
-            ),
+              Expanded(
+                child: dataWidget('Data:', '_'),
+              ),
+            ],
           ),
-          Divider(
-            thickness: 1,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: dataWidget('Transaction Code:', 'RINO9803'),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Expanded(
+                child: dataWidget('Processed By:', 'rinnohara9803@gmail.com'),
+              ),
+            ],
           ),
-          Expanded(
-            flex: 5,
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 5,
-                horizontal: 10,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: dataWidget('Is Debit:', 'True'),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: dataWidget('Channel:', 'GPRS'),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Expanded(
-                        child: dataWidget('Data:', '_'),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: dataWidget('Transaction Code:', 'RINO9803'),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Expanded(
-                        child: dataWidget(
-                            'Processed By:', 'rinnohara9803@gmail.com'),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: dataWidget('Is Debit:', 'True'),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Expanded(
-                        child: dataWidget('', ''),
-                      ),
-                    ],
-                  ),
-                ],
+              SizedBox(
+                width: 5,
               ),
-            ),
-          ),
-          Divider(
-            thickness: 1,
-          ),
-          Expanded(
-            flex: 5,
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 5,
-                horizontal: 10,
+              Expanded(
+                child: dataWidget('', ''),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: dataWidget('Purpose:', 'Personal Use'),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Expanded(
-                        child: dataWidget(
-                            'Receiver Username:', 'Swornima Shrestha'),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: dataWidget('Email:', 'swornimastha@gmail.com'),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Expanded(
-                        child: dataWidget('Sender Name:', 'Ajay Prajapti'),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Expanded(
-                        child: dataWidget('Payment Method:', 'mhicha Ewallet'),
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Expanded(
-                        child: dataWidget('Remarks:', 'due'),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+            ],
           ),
         ],
       ),
     );
-
-    return PdfApi.saveDocument(name: 'student1.pdf', pdf: pdf);
   }
 
-  // static Widget buildInvoiceImage(
-  //   Student student,
-  // ) {
-  //   return MemoryImage(
-  //     (rootBundle.load('assets/img/your_image.jpg')).buffer.asUint8List(),
-  //   );
-  // }
-
-  static Widget buildTop(student) => Column(
+  static buildBottomInvoiceLayout() {
+    Widget dataWidget(String key, String value) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            student.name,
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.center,
+            key,
+          ),
+          Text(
+            value,
+            maxLines: 1,
           ),
         ],
       );
+    }
+
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 5,
+        horizontal: 10,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: dataWidget('Purpose:', 'Personal Use'),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Expanded(
+                child: dataWidget('Receiver Username:', 'Swornima Shrestha'),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: dataWidget('Email:', 'swornimastha@gmail.com'),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Expanded(
+                child: dataWidget('Sender Name:', 'Ajay Prajapti'),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: dataWidget('Payment Method:', 'mhicha Ewallet'),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Expanded(
+                child: dataWidget('Remarks:', 'due'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
